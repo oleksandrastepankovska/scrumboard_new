9d7b936839e9e1417d445f5d548bf1419d05b621
Oleksandra Stepankovska <oleksandra.st3@gmail.com>
2021-02-18 23:18:10 +0200

    extended seed data

--- TaskBoard/TaskBoard/Data/Migrations/Configuration.cs
+++ TaskBoard/TaskBoard/Data/Migrations/Configuration.cs
@@ -19,8 +19,12 @@
                 new Status { Id = 2, Name = "In proggress" },
                 new Status { Id = 3, Name = "Done" });
 
-            var person = new Person { FirstName = "Olexandra", LastName = "Stepankovska" };
-            context.Person.AddOrUpdate(person);
+            var personOlexandra = new Person { Id = 1, FirstName = "Olexandra", LastName = "Stepankovska" };
+            var personSanya = new Person { Id = 2, FirstName = "Sanya", LastName = "Belyi" };
+            var personOlexandr = new Person { Id = 3, FirstName = "Olexandr", LastName = "Makedonskyi" };
+            context.Person.AddOrUpdate(personOlexandra);
+            context.Person.AddOrUpdate(personSanya);
+            context.Person.AddOrUpdate(personOlexandr);
 
             context.Project.AddOrUpdate(
                 new Project
@@ -28,7 +32,12 @@
                     Id = 1,
                     Name = "Task Board",
                     Description = "Examination project in university",
-                    Assignees = new List<Person>() { person }
+                    Assignees = new List<Person>() { personOlexandra }
+                },
+                new Project { 
+                    Id = 2,
+                    Name = "Brigada",
+                    Description = "Recketing gang"
                 });
 
             context.Assignment.AddOrUpdate(
@@ -38,8 +47,25 @@
                     Name = "Pass examination",
                     Description = string.Empty,
                     StatusId = 2,
-                    AssigneeId = 1,
+                    AssigneeId = personOlexandra.Id,
                     ProjectId = 1
+                },
+                new Assignment { 
+                    Id = 2,
+                    Name = "Finish program",
+                    Description = string.Empty,
+                    StatusId = 3,
+                    AssigneeId = personOlexandr.Id,
+                    ProjectId = 1
+                },
+                new Assignment
+                {
+                    Id = 3,
+                    Name = "Earn money and respect",
+                    Description = "Find a way to earn money and respect",
+                    StatusId = 2,
+                    AssigneeId = personSanya.Id,
+                    ProjectId = 2
                 });
         }
     }
